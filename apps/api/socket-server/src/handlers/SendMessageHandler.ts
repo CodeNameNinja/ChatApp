da5667dacae18910d2e7cbd0d22bb75d62f49c0f
Mitchell Yuen <mwhyuen@gmail.com>
2024-03-23 21:11:47 +0200

    feat: automatically set sender as the username from token

--- apps/api/socket-server/src/handlers/SendMessageHandler.ts
+++ apps/api/socket-server/src/handlers/SendMessageHandler.ts
@@ -1,18 +1,27 @@
+// SendMessageHandler.ts
 import { Socket } from 'socket.io';
 import IHandler from '@interfaces/IHandler';
 import { Message } from '@models/message';
 import { AuthenticationService } from '@services/AuthenticationService';
+import jwt from 'jsonwebtoken';
 
 export class SendMessageHandler implements IHandler {
-    async handle(socket: Socket, data: { text: string; sender: string; token: string }): Promise<void> {
+    async handle(socket: Socket, data: { text: string; token: string }): Promise<void> {
         const verificationResult = AuthenticationService.verifyToken(data.token);
-        if (typeof verificationResult === 'string') {
-            socket.emit('error', { message: 'Authentication failed' });
-            return;
-        }
 
-        const message = new Message({ text: data.text, sender: data.sender });
-        await message.save();
-        socket.to('mainRoom').emit('new_message', message);
+        if (typeof verificationResult !== 'string') {
+            const username = (verificationResult as jwt.JwtPayload).username;
+
+            if (!username) {
+                socket.emit('error', { message: 'Authentication failed: Username not found in token' });
+                return;
+            }
+
+            const message = new Message({ text: data.text, sender: username });
+            await message.save();
+            socket.to('mainRoom').emit('new_message', message);
+        } else {
+            socket.emit('error', { message: 'Authentication failed' });
+        }
     }
 }
