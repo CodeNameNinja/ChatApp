61d5525a260c0359d55cd85ab64d4872b6e56ba1
Mitchell Yuen <mwhyuen@gmail.com>
2024-03-24 10:18:57 +0200

    feat: add LoginHandler.test.ts

---
+++ apps/api/socket-server/src/handlers/__test__/LoginHandler.test.ts
@@ -0,0 +1,55 @@
+import {Socket} from 'socket.io';
+import {AuthenticationService} from '@services/AuthenticationService';
+
+import {mocked} from 'jest-mock';
+import {LoginHandler} from "@handlers/LoginHandler";
+
+
+jest.mock('@services/AuthenticationService', () => ({
+    AuthenticationService: {
+        handleLogin: jest.fn()
+    }
+}));
+beforeEach(() => {
+    mocked(AuthenticationService.handleLogin).mockClear();
+});
+
+describe('LoginHandler', () => {
+    let loginHandler: LoginHandler;
+    let mockSocket: Partial<Socket>;
+
+    beforeEach(() => {
+        loginHandler = new LoginHandler();
+        mockSocket = {
+            emit: jest.fn()
+        };
+    });
+
+    it('should emit login_success on successful login', async () => {
+        // Setup
+        const testCredentials = {username: 'testUser', password: 'testPass'};
+        mocked(AuthenticationService.handleLogin).mockReturnValue({
+            success: true,
+            message: 'Success',
+            token: 'fakeToken'
+        });
+
+        // Action
+        await loginHandler.handle(mockSocket as Socket, testCredentials);
+
+        // Assert
+        expect(mockSocket.emit).toHaveBeenCalledWith('login_success', {message: 'Success', token: 'fakeToken'});
+    });
+
+    it('should emit login_failed on failed login', async () => {
+        // Setup
+        const testCredentials = {username: 'wrongUser', password: 'wrongPass'};
+        mocked(AuthenticationService.handleLogin).mockReturnValue({success: false, message: 'Failed'});
+
+        // Action
+        await loginHandler.handle(mockSocket as Socket, testCredentials);
+
+        // Assert
+        expect(mockSocket.emit).toHaveBeenCalledWith('login_failed', {message: 'Failed'});
+    });
+});
